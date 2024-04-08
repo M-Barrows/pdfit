7be6b2ff71bbcc6dc9a8761cef1414def07ef97c
M-Barrows <michael.barrows.a@gmail.com>
2024-04-08 12:57:35 -0400

    basic pdf construction working

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from src.libs import create_pdf
+from logging.config import dictConfig
+
 def create_app():
+    dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+    
     app = Flask(__name__)
 
     @app.route("/", methods=["GET"])
@@ -9,12 +27,16 @@ def create_app():
 
     @app.route("/create", methods=["GET","POST"])
     def create_doc(): 
-        # create_pdf() 
-        return render_template("index.html"),200
+        if request.method == "POST":
+            urls = request.form.getlist("url")
+            create_pdf(urls)
+            return render_template("form-data.html",urls=request.form.getlist("url")),200
+        else:
+            return render_template("index.html"),200
 
     @app.route("/add-image", methods=["GET","POST"])
     def add_image():
-        return "<div><input type='text'></div>",200
+        return "<div><input name='url' type='text'></div>",200
 
     return app
 
--- src/libs.py
+++ src/libs.py
@@ -1,5 +1,24 @@
 from fpdf import FPDF
 import pathlib
+import requests
+import shutil
+import secrets
+import logging
+
+
+def download_image(url:str):
+    response = requests.get(url, headers={'User-Agent': 'PDF-it'}, stream = True)
+    if response.status_code == 200:
+        file_name = secrets.token_hex(10)
+        file_type = url.split('.')[-1]
+        file_name = f"image-cache/{file_name}.{file_type}"
+        with open(file_name,"wb") as f:
+            shutil.copyfileobj(response.raw, f)
+        logging.info(f"Successfully downloaded image from {url}")
+        return file_name
+    else: 
+        logging.warn(f"⛔ Could not download image from {url}. Skipping.")
+        return None
 
 def create_pdf(images=list()):
     if len(images) < 1:
@@ -9,11 +28,12 @@ def create_pdf(images=list()):
         images_per_page = 2
         for i, image in enumerate(images,1):
             print(image)
+            img_file_path = download_image(image)
             page_image_number = images_per_page  if (i % images_per_page) == 0 else (i % images_per_page)
             img_height = 10/images_per_page
             if page_image_number == 1:
                 pdf.add_page()
-                pdf.image(str(image),x=0,y=0,w=0,h=img_height)
+                pdf.image(str(img_file_path),x=0,y=0,w=0,h=img_height)
             else:
-                pdf.image(str(image),x=None,y=(page_image_number-1)*img_height,w=0,h=img_height)
+                pdf.image(str(img_file_path),x=None,y=(page_image_number-1)*img_height,w=0,h=img_height)
         pdf.output("pdfit_output.pdf", "F")
